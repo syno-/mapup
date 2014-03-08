e77e203b7380cec13a254c1bd31795eab77a54ab
esperia <esperia09@gmail.com>
2014-03-09 00:18:17 +0900

    Added Geolocation API code

---
+++ example/js/index.js
@@ -0,0 +1,56 @@
+
+
+//reqLocation().done(function(ll) {
+//}).fail(function(e) {
+//});
+function reqLocation() {
+    var d = $.Deferred();
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            d.done.apply(d, arguments);
+        }, function() {
+            d.fail.apply(d, arguments);
+        });
+    } else {
+        d.fail();
+    }
+
+    return d.promise();
+}
+
+function map() {
+    var mapOptions = {
+        center: new google.maps.LatLng(34.701909, 135.494977),
+        zoom: 12,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map($('#map_canvas')[0], mapOptions);
+
+    var marker = new google.maps.Marker({
+        position: map.getCenter(),
+        map: map,
+        title: 'Click to zoom'
+    });
+
+    var infowindow = new google.maps.InfoWindow({
+        content: 'Info Window',
+        size: new google.maps.Size(50,50)
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+        map.setZoom(12);
+        map.setCenter(marker.getPosition());
+
+        infowindow.open(map, marker);
+    });
+}
+
+$(function() {
+    map();
+    reqLocation().done(function(pos) {
+        alert('detected: ' + pos);
+    }).fail(function() {
+        alert('detect fail');
+    });
+});
